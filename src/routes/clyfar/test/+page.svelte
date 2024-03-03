f64e36472f60a0d138fc92660785ca61b98ea34e
okuruu <Phantom_Ghost@outlook.co.id>
2024-03-03 15:38:59 +0700

    Added MSDT

--- src/routes/clyfar/test/+page.svelte
+++ src/routes/clyfar/test/+page.svelte
@@ -4,9 +4,11 @@
     import Kraeplin from "$lib/components/clyfar/Kraeplin.svelte";
     import Baum from "$lib/components/clyfar/Baum.svelte";
     import Mbti from "$lib/components/clyfar/MBTI.svelte";
+    import Msdt from "$lib/components/clyfar/MSDT.svelte";
 </script>
 <!-- <DISC/> -->
 <!-- <Papi/> -->
 <!-- <Kraeplin/> -->
 <!-- <Baum/> -->
 <!-- <Mbti/> -->
+<Msdt/>
