6a59e76ccb6ce53c19000f39aed380fd8aebd474
okuruu <Phantom_Ghost@outlook.co.id>
2024-03-10 19:03:43 +0700

    Naming Correction

--- src/routes/clyfar/test/+page.svelte
+++ src/routes/clyfar/test/+page.svelte
@@ -5,12 +5,12 @@
     import { baseConfig } from "$lib/strings/baseConfig";
 
     import DISC from "$lib/components/clyfar/DISC.svelte";
-    import Papi from "$lib/components/clyfar/Papi.svelte";
     import Baum from "$lib/components/clyfar/Baum.svelte";
-    import Mbti from "$lib/components/clyfar/MBTI.svelte";
-    import Msdt from "$lib/components/clyfar/MSDT.svelte";
+    import MBTI from "$lib/components/clyfar/MBTI.svelte";
+    import MSDT from "$lib/components/clyfar/MSDT.svelte";
     import CFIT from "$lib/components/clyfar/CFIT.svelte";
     import Kraeplin from "$lib/components/clyfar/Kraeplin.svelte";
+    import Papikostick from "$lib/components/clyfar/Papkostick.svelte";
     
     onMount(() => {
         const getStorage = getLocalStorage();
@@ -24,15 +24,15 @@
 {#if $baseConfig.currentTest === 'DISC'}
     <DISC/>
 {:else if $baseConfig.currentTest === 'PAPI'}
-    <Papi/>
+    <Papikostick/>
 {:else if $baseConfig.currentTest === 'KRAEPLIN'}
     <Kraeplin/>
 {:else if $baseConfig.currentTest === 'BAUM'}
     <Baum/>
 {:else if $baseConfig.currentTest === 'MBTI'}
-    <Mbti/>
+    <MBTI/>
 {:else if $baseConfig.currentTest === 'MSDT'}
-    <Msdt/>
+    <MSDT/>
 {:else if $baseConfig.currentTest === 'CFIT'}
     <CFIT/>
 {/if}
