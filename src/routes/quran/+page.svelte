bebf65b7e8a167c03eb69de045893775e79c378b
okuruu <Phantom_Ghost@outlook.co.id>
2024-05-19 16:36:05 +0700

    Adding Quicklinks and save last read on Quran

--- src/routes/quran/+page.svelte
+++ src/routes/quran/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+    import toast, { Toaster } from 'svelte-french-toast';
     import NavigationBar from "$lib/components/NavigationBar.svelte";
+    import { goto } from '$app/navigation';
+    
     export let data;
 
     interface Quran { 
@@ -31,13 +34,33 @@
             searchFocus();
         }
     }
+
+    function lastRead(){
+        const getLocalStorage = localStorage.getItem('lastRead');
+        if(getLocalStorage == null){
+            toast('Kamu belum menandai ayat terakhir!', { icon: '👏', style: 'border-radius: 200px; background: #333; color: #fff;' });
+            return;
+        }
+        const lastRead: { surahName: number; lastAyah: number } = JSON.parse(getLocalStorage);
+        return goto(`/quran/${lastRead.surahName}#${lastRead.lastAyah}`);
+    }
 </script>
+<Toaster/>
 <div class="container mx-auto p-2">
-    <label class="input input-bordered flex items-center gap-2 mt-3">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
-        <input type="text" bind:this={searchBar} on:keyup={searchSurah} class="grow" placeholder="Search" />
-        <kbd class="kbd kbd-sm">Esc</kbd> to search.
-    </label>
+    <div class="grid grid-cols-5 gap-4">
+        <div class="col-span-4">
+            <label class="input input-bordered flex items-center gap-2 mt-3">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" /></svg>
+                <input type="text" bind:this={searchBar} on:keyup={searchSurah} class="grow" placeholder="Search" />
+            </label>
+        </div>
+        <div class="col-span-1">
+            <button type="button" on:click={lastRead} class="btn shadow-xl w-full mt-3">
+                <svg width="18" height="18" viewBox="0 0 48 48" class="text-orange-400 h-5 w-5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 7H16C20.4183 7 24 10.5817 24 15V42C24 38.6863 21.3137 36 18 36H5V7Z" fill="none" stroke="currentColor" stroke-width="4" stroke-linejoin="bevel"></path><path d="M43 7H32C27.5817 7 24 10.5817 24 15V42C24 38.6863 26.6863 36 30 36H43V7Z" fill="none" stroke="currentColor" stroke-width="4" stroke-linejoin="bevel"></path></svg>
+            </button>
+        </div>
+    </div>
+
 
     {#if qurans.length === 0}
         <div class="card w-full bg-base-100 shadow-xl my-7">
--- src/routes/quran/[chapters]/+page.svelte
+++ src/routes/quran/[chapters]/+page.svelte
@@ -1,21 +1,64 @@
 <script lang="ts">
     import NavigationBar from "$lib/components/NavigationBar.svelte";
 
-    export let data;
+    // Define the types for the data prop and local variables
+    export let data: {
+        pageChapter: string;
+        chapterContents: { arabic: string; transliteration: string }[];
+    };
+
+    let ayah: number | null = null;
+    let isModal: boolean = false;
+
+    // Open modal and set the current ayah index
+    function openModal(id: number) {
+        ayah = id;
+        isModal = true;
+    }
+
+    // Close modal and reset the ayah
+    function closeModal() {
+        ayah = null;
+        isModal = false;
+    }
+
+    // Save the last read ayah to localStorage
+    function bookmark() {
+        const lastRead = {
+            surahName: Number(data.pageChapter),
+            lastAyah: ayah,
+        };
+
+        localStorage.setItem('lastRead', JSON.stringify(lastRead));
+        closeModal();
+    }
 </script>
-<NavigationBar/>
+
+<NavigationBar />
 <div class="container mx-auto">
     <div class="max-w-2xl mx-auto p-4">
-        <!-- Chapter Contents -->
         <div class="space-y-4">
-            {#each data.chapterContents as verse,index}
-                <div class="rounded-lg p-4">
-                    <!-- Arabic Text -->
-                    <div class="text-2xl mb-2 text-right rtl arabic pre-line">{ verse.arabic }</div>
-                    <!-- Transliteration -->
-                    <div class="text-gray-600 mb-2 mt-5 rtl text-right">{index + 1}. { verse.transliteration }</div>
-                </div>
+            {#each data.chapterContents as verse, index}
+                <button id="{index.toString()}" on:click={() => openModal(index)} class="rounded-lg p-4 text-left w-full">
+                    <div class="text-2xl mb-2 text-right rtl arabic pre-line">{verse.arabic}</div>
+                </button>
+                <div class="text-gray-600 mb-2 mt-5 rtl text-right">{index + 1}. {verse.transliteration}</div>
             {/each}
         </div>
     </div>
 </div>
+
+<!-- Modal -->
+{#if isModal}
+    <div class="modal modal-open">
+        <div class="modal-box">
+            <h3 class="font-bold text-lg">Tandai Ayat Terakhir!</h3>
+            <button type="button" on:click={closeModal} class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            <p class="py-4">Dengan menandai ayat ini sebagai yang terakhir, kamu akan mudah menemukannya untuk dibaca lagi nanti.</p>
+            <div class="modal-action">
+                <button type="button" class="btn" on:click={bookmark}>Ya, Tandai</button>
+            </div>
+        </div>
+        <button on:click={closeModal} class="modal-backdrop"></button>
+    </div>
+{/if}
