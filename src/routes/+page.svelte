d1749bc16c8a13597c98093ee8aad2be935f244a
okuruu <Phantom_Ghost@outlook.co.id>
2024-07-10 22:15:19 +0700

    Changing to Bootstrap 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-aaa
+<div class="bg-dark vh-100 wh-100">
+    <div class="container-sm">
+
+        <div class="row mt-7">
+            <div class="col">
+                <div class="d-flex justify-content-end">
+                    <img src="/images/avatar.jfif" alt="" class="avatar h-125px" />
+                </div>
+            </div>
+            <div class="col mt-5">
+                <h1 class="text-white">Richie</h1>
+                <span class="text-white">Lórien Loremaster of Languages</span>
+                <p class="text-muted">@okuruu</p>
+            </div>
+        </div>
+
+        <div class="my-5"></div>
+
+        <h1 class="font-bold text-white">About</h1>
+        <p class="mt-2 text-white">Hey there, I'm Gilby, a Fullstack Developer on a mission to turn ideas into digital delights. Armed with my trusty keyboard and fueled by endless snacks, I tame bugs and craft user experiences that spark joy.</p>
+
+        <div class="my-5"></div>
+
+
+    </div>
+</div>
