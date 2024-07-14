3520a1b3e6cc2a7dd3d09b348de7d2fe4ec4e8ac
okuruu <Phantom_Ghost@outlook.co.id>
2024-07-14 23:25:20 +0700

    Added Thread hints!

--- src/routes/threads/[slugs]/+page.svelte
+++ src/routes/threads/[slugs]/+page.svelte
@@ -1,3 +1,59 @@
+<script lang="ts">
+    import type { Threads } from '../../../library/interface/Threads.js';
+    import { formatLike } from '../../../library/utils/formatLike';
+    import { formatTime } from '../../../library/utils/formatTime';
+
+    export let data;
+    const threads: Threads = data.threads;
+</script>
 <div class="container-xs">
-    aaa
+    
+    <div class="row">
+        <div class="col-2">
+            <img src="/images/avatar.jfif" alt="" class="avatar h-30px" />
+        </div>
+        <div class="col-10 text-white">
+            <div class="form-group">
+                <b>gdydz</b>
+                <img src="/icons/elements/Verified.svg" alt="">
+                <span class="text-muted ms-1">{formatTime(threads.timestamp)}</span>
+            </div>
+            <p class="mt-2">{threads.tweets}</p>
+
+            <div class="form-group">
+                <img src="/icons/elements/Heart.svg" class="h-15px cursor-not-allowed" alt="Likes"> <span class="me-3 ms-1">{formatLike(threads.likes)}</span>
+                <img src="/icons/elements/Comment.svg" class="h-15px cursor-not-allowed" alt="Comments">  <span class="me-3 ms-1">-</span>
+                <img src="/icons/elements/Reply.svg" class="h-15px cursor-not-allowed" alt="Replies">  <span class="me-3 ms-1">-</span>
+                <img src="/icons/elements/Share.svg" class="h-15px cursor-not-allowed" alt="Share">  <span class="me-3 ms-1">-</span>
+            </div>
+        </div>
+        <div class="separator opacity-25 secondary my-7"></div>
+        {#if threads.reply.replies.length !== 0}
+            {#each threads.reply.replies as replies, indexes }
+                <div class="row">
+                    <div class="col-2">
+                        <img src="/images/avatar.jfif" alt="" class="avatar h-30px" />
+                    </div>
+                    <div class="col-10 text-white">
+                        <div class="form-group">
+                            <b>gdydz</b>
+                            <img src="/icons/elements/Verified.svg" alt="">
+                            <span class="text-muted ms-1">{formatTime(threads.timestamp)}</span>
+                        </div>
+                        <p class="mt-2">{replies.tweets}</p>
+            
+                        <p class="text-muted">
+                            {#if indexes === threads.reply.replies.length - 1}
+                                - End of thread.
+                            {:else}
+                                - Thread replies.
+                            {/if}
+                        </p>
+                        
+                    </div>
+                    <div class="separator opacity-25 secondary my-7"></div>
+                </div>
+            {/each}
+        {/if}
+    </div>
 </div>
--- src/routes/threads/[slugs]/+page.ts
+++ src/routes/threads/[slugs]/+page.ts
@@ -1,5 +1,7 @@
+import { threadsTweets } from '../../../library/threads.js'
+
 export const load = ({params}) => {
     return {
-        threads : []
+        threads : threadsTweets[Number(params.slugs)]
     }
 }
