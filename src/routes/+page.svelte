cd9778ee2f7dcb37f692b7d8ee18ee08c6d294c3
okuruu <Phantom_Ghost@outlook.co.id>
2024-07-14 20:55:59 +0700

    Updating Frontpage with icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,51 @@
 <div class="bg-dark vh-100 wh-100">
-    <div class="container-sm">
+    <div class="container-xs">
 
-        <div class="row mt-7">
-            <div class="col">
-                <div class="d-flex justify-content-end">
-                    <img src="/images/avatar.jfif" alt="" class="avatar h-125px" />
-                </div>
+        <!-- The Navbar? -->
+        <div class="navbar d-flex justify-between-end mb-7">
+            <div class="form-group">
+                <img src="/icons/elements/Function.svg" alt="Site Icon" class="h-30px" />
             </div>
-            <div class="col mt-5">
+            <div class="form-group">
+                <a href="https://github.com/okuruu">
+                    <img src="/icons/Github.svg" class="h-20px me-2" alt="Github Icon"/>
+                </a>
+                <img src="/icons/Quran.svg" class="h-20px me-2" alt="Quran Icon"/>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-between">
+            <img src="/images/avatar.jfif" alt="" class="avatar h-100px" />
+            <div class="form-group mt-4">
                 <h1 class="text-white">Richie</h1>
                 <span class="text-white">Lórien Loremaster of Languages</span>
                 <p class="text-muted">@okuruu</p>
             </div>
         </div>
 
-        <div class="my-5"></div>
+        <div class="d-flex justify-content-center">
+            <div class="separator w-25 my-7"></div>
+        </div>
 
+        <!-- Bio -->
         <h1 class="font-bold text-white">About</h1>
-        <p class="mt-2 text-white">Hey there, I'm Gilby, a Fullstack Developer on a mission to turn ideas into digital delights. Armed with my trusty keyboard and fueled by endless snacks, I tame bugs and craft user experiences that spark joy.</p>
+        <p class="mt-2 text-muted">Hey there, I'm Gilby, a Fullstack Developer on a mission to turn ideas into digital delights. Armed with my trusty keyboard and fueled by endless snacks, I tame bugs and craft user experiences that spark joy. Also, i love maths!</p>
 
-        <div class="my-5"></div>
+        <div class="separator my-5"></div>
+
+        <!-- Currently At -->
+        <div class="row text-white">
+            <div class="col-4">
+                <b>Dea Bakery - Present</b>
+            </div>
+            <div class="col-8">
+                <b>Head of Information Technology</b>
+                <ul class="text-muted">
+                    <li>Led development team</li>
+                    <li>Building POS, Internal Management System, and various other projects</li>
+                </ul>
+            </div>
+        </div>
 
 
     </div>
