ac670d3c51e6cd87fbb2b9f4555fa7569a88899a
okuruu <Phantom_Ghost@outlook.co.id>
2024-07-14 22:32:57 +0700

    Adding Quran Bookmark

--- src/routes/quran/+page.svelte
+++ src/routes/quran/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { goto } from '$app/navigation';
+    import toast, { Toaster } from 'svelte-french-toast';
     import type { Chapters } from '../../library/interface/Quran.js';
 
     export let data;
@@ -24,14 +26,25 @@
         });
         quranChapters = searchSurah;
     }
+
+    function lastRead(){
+        const getLocalStorage = localStorage.getItem('lastRead');
+        if(getLocalStorage == null){
+            toast('Kamu belum menandai ayat terakhir!', { icon: '👏', style: 'border-radius: 200px; background: #333; color: #fff;' });
+            return;
+        }
+        const lastRead: { surahName: number; lastAyah: number } = JSON.parse(getLocalStorage);
+        return goto(`/quran/${lastRead.surahName}#${lastRead.lastAyah}`);
+    }
 </script>
+<Toaster/>
 <div class="container-xs">
     <div class="row ">
         <div class="col-9">
             <input type="text" bind:this={searchBar} on:keyup={searchSurah} class="form-control form-control-transparent bg-dark text-white shadow" placeholder="[ESC] Cari Ayat" />
         </div>
         <div class="col-3">
-            <button type="button" class="btn btn-icon shadow-sm text-white w-100">
+            <button type="button" on:click={lastRead} class="btn btn-icon shadow-sm text-white w-100">
                 <svg width="18" height="18" viewBox="0 0 48 48" class="text-orange-400 h-5 w-5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 7H16C20.4183 7 24 10.5817 24 15V42C24 38.6863 21.3137 36 18 36H5V7Z" fill="none" stroke="currentColor" stroke-width="4" stroke-linejoin="bevel"></path><path d="M43 7H32C27.5817 7 24 10.5817 24 15V42C24 38.6863 26.6863 36 30 36H43V7Z" fill="none" stroke="currentColor" stroke-width="4" stroke-linejoin="bevel"></path></svg>
             </button>
         </div>
--- src/routes/quran/[chapters]/+page.svelte
+++ src/routes/quran/[chapters]/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
     export let data;
+    import toast, { Toaster } from 'svelte-french-toast';
+
+    async function setBookmark(aya: number) {
+        const lastRead = {
+            surahName: Number(data.pageChapter),
+            lastAyah: aya,
+        };
+
+        localStorage.setItem('lastRead', JSON.stringify(lastRead));
+        toast.success("Bookmark telah disimpan.")
+    }
 </script>
+<Toaster/>
 <div class="container-xs">
     {#each data.chapterContents as verse, index}
-        <div class="form-group">
+    <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions (because of reasons) -->
+        <div id="{index.toString()}" class="form-group" on:click={() => setBookmark(index)}>
             <p class="text-muted text-end quran-font display-5 fw-bolder">{verse.arabic}</p>
             <p class="text-gray-600 text-end">{index + 1}. {verse.transliteration}</p>
         </div>
