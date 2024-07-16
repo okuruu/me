727df8f61c9729bd091793aee588777f0478705e
okuruu <Phantom_Ghost@outlook.co.id>
2024-07-16 20:31:11 +0700

    Porting: MSDT Example

--- src/routes/clyfar/test/+page.svelte
+++ src/routes/clyfar/test/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     // Todo
     import ExampleDisc from "../../../library/components/clyfar/sample/ExampleDISC.svelte";
+    import ExampleMsdt from "../../../library/components/clyfar/sample/ExampleMSDT.svelte";
     import ExamplePapi from "../../../library/components/clyfar/sample/ExamplePapi.svelte";
 </script>
-<ExampleDisc/>
+<ExampleMsdt/>
