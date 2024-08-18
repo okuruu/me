1be3728a7de5a57f569daec220f2fcbf04a6bc0f
okuruu <Phantom_Ghost@outlook.co.id>
2024-08-18 13:22:17 +0700

    Interpretasi MBTI Done

---
+++ src/library/components/clyfar/interpretation/InterpretationMBTI.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let mbti;
+</script>
+<div class="card shadow-sm bg-white my-5">
+    <div class="card-header">
+        <h3 class="card-title fw-bolder">Interpretasi MBTI</h3>
+        <div class="card-toolbar">
+            <button type="button" class="btn btn-sm btn-light">
+                Interpretation Viewer
+            </button>
+        </div>
+    </div>
+    <div class="card-body">
+        Tipe MBTI testee adalah: <span class="fw-bolder">{mbti}</span>
+    </div>
+</div>
--- src/routes/clyfar/beranda/[id]/+page.svelte
+++ src/routes/clyfar/beranda/[id]/+page.svelte
@@ -4,6 +4,7 @@
     import InterpretationPapikostick from '../../../../library/components/clyfar/interpretation/InterpretationPapikostick.svelte';
     import InterpretationMsdt from '../../../../library/components/clyfar/interpretation/InterpretationMSDT.svelte';
     import InterpretationDisc from '../../../../library/components/clyfar/interpretation/InterpretationDISC.svelte';
+    import InterpretationMbti from '../../../../library/components/clyfar/interpretation/InterpretationMBTI.svelte';
 
     export let data: any;
     let kraeplin: Kraeplin = data.result.KRAEPLIN ?? {};
@@ -11,10 +12,12 @@
     let papikostickTable: { NILAI : number; Deskripsi: string; }[] = data.result.PAPI_TABLE;
     let msdt: MSDT = data.result.MSDT ?? {};
     let disc: DISC = data.result.DISC ?? {};
+    let mbti: string = data.result.MBTI ?? "";
     console.log(data)
 </script>
 <div class="bg-clyfar">
     <div class="container-sm">
+        <InterpretationMbti mbti={mbti}/>
         <InterpretationDisc disc={disc} />
         <InterpretationMsdt msdt={msdt}/>
         <InterpretationKraeplin kraeplin={kraeplin} />
